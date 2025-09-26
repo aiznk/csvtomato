3c1c17ae7da12cf33ca393c1c9c54079451aa1ad
aiznk <nothing@nothing>
2025-09-26 18:14:18 +0900

    ok

--- shell.c
+++ shell.c
@@ -1,11 +1,20 @@
 #include <csvtomato.h>
 
-int main(void) {
-	char cmd[1024];
+int main(int argc, char *argv[]) {
 	const char *db_dir = "test_db";
+	if (argc >= 2) {
+		db_dir = argv[1];
+	}
+
+	if (!csvtmt_file_exists(db_dir)) {
+		fprintf(stderr, "can't open database. %s does not exists.\n", db_dir);
+		return 1;
+	}
+
+	char cmd[1024];
 
 	for (;;) {
-		printf("> ");
+		printf("%s > ", db_dir);
 		fflush(stdout);
 
 		if (!fgets(cmd, sizeof cmd, stdin)) {
